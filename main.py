01e71a6f2d6529b12d4fe3c8fd89f33bd72b86bf
morbid240 <162398707+morbid240@users.noreply.github.com>
2024-06-12 05:27:23 -0700

    Update main.py

--- main.py
+++ main.py
@@ -6,13 +6,23 @@ from db_connection import engine, Session
 from orm_base import metadata
 # Note that until you import your SQLAlchemy declarative classes, such as Student, Python
 # will not execute that code, and SQLAlchemy will be unaware of the mapped table.
-
+from Department import Department
+from Course import Course
+from Section import Section
 from Option import Option
 from Menu import Menu
 # Poor man's enumeration of the two available modes for creating the tables
 from constants import START_OVER, INTROSPECT_TABLES, REUSE_NO_INTROSPECTION
 from sqlalchemy import inspect  # map from column name to attribute name
 
+
+
+def add_department(session):
+    Department.add_department()
+def select_department(session):
+    Department.select_department()
+def delete_department(session):
+    Department.delete_department()
 
 
 if __name__ == '__main__':
