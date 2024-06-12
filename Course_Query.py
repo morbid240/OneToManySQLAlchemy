f639cd73a0294d84bce8f8de77ec15cd6fb6cce8
morbid240 <162398707+morbid240@users.noreply.github.com>
2024-06-12 07:40:18 -0700

    Create Course_Query.py

---
+++ Course_Query.py
@@ -0,0 +1,73 @@
+from Course import Course
+
+def add_course(session):
+    """
+    Prompt the user for the information for a new course and validate
+    the input to make sure that we do not create any duplicates.
+    :param session: The connection to the database.
+    :return:        None
+    """
+    print("Which department offers this course?")
+    department: Department = select_department(sess)
+    unique_number: bool = False
+    unique_name: bool = False
+    number: int = -1
+    name: str = ''
+    while not unique_number or not unique_name:
+        name = input("Course full name--> ")
+        number = int(input("Course number--> "))
+        name_count: int = session.query(Course).filter(Course.departmentAbbreviation == department.abbreviation,
+                                                       Course.name == name).count()
+        unique_name = name_count == 0
+        if not unique_name:
+            print("We already have a course by that name in that department.  Try again.")
+        if unique_name:
+            number_count = session.query(Course). \
+                filter(Course.departmentAbbreviation == department.abbreviation,
+                       Course.courseNumber == number).count()
+            unique_number = number_count == 0
+            if not unique_number:
+                print("We already have a course in this department with that number.  Try again.")
+    description: str = input('Please enter the course description-->')
+    units: int = int(input('How many units for this course-->'))
+    course = Course(department, number, name, description, units)
+    session.add(course)
+    session.commit()
+
+
+
+def select_course(sess) -> Course:
+    """
+    Select a course by the combination of the department abbreviation and course number.
+    Note, a similar query would be to select the course on the basis of the department
+    abbreviation and the course name.
+    :param sess:    The connection to the database.
+    :return:        The selected student.
+    """
+    found: bool = False
+    department_abbreviation: str = ''
+    course_number: int = -1
+    while not found:
+        department_abbreviation = input("Department abbreviation--> ")
+        course_number = int(input("Course Number--> "))
+        name_count: int = sess.query(Course).filter(Course.departmentAbbreviation == department_abbreviation,
+                                                    Course.courseNumber == course_number).count()
+        found = name_count == 1
+        if not found:
+            print("No course by that number in that department.  Try again.")
+    course = sess.query(Course).filter(Course.departmentAbbreviation == department_abbreviation,
+                                       Course.courseNumber == course_number).first()
+    return course
+
+
+
+def delete_course(session):
+    """
+    Prompt the user to select a course by department abbreviation and course number, then delete it.
+    :param session: The connection to the database.
+    :return: None
+    """
+    print("Deleting a course")
+    course = select_course(session)
+    session.delete(course)
+    session.commit()
