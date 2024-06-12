9617bb284c40f4754b45822aba02d44955394d52
morbid240 <162398707+morbid240@users.noreply.github.com>
2024-06-12 08:46:50 -0700

    Update Course_Query.py

--- Course_Query.py
+++ Course_Query.py
@@ -69,8 +69,10 @@ def delete_course(session):
     """
     print("Deleting a course")
     course = select_course(session)
-    session.delete(course)
-    session.commit()
+    if 0 < session.query(Sections).filter(Section.courseNumber == course.courseNumber).count()
+        print("Sections depend on this course, go delete them first and try again.")
+    else:
+        session.delete(course)
 
 def list_courses(sess):
     """
