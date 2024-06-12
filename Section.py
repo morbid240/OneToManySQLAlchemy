415f8ed2a0535ef7eba654ef0c4928cd8abb31ca
morbid240 <162398707+morbid240@users.noreply.github.com>
2024-06-12 11:08:44 -0700

    Update Section.py

--- Section.py
+++ Section.py
@@ -23,53 +23,48 @@ table_name: str = "sections"  # The physical name of this table
 introspection_type = IntrospectionFactory().introspection_type
 
 
-    class Section(Base):
-        __tablename__ = table_name  # Give SQLAlchemy the name of the table.
-        # PRIMARY KEYS
-        departmentAbbreviation: Mapped[str] = mapped_column('department_abbreviation', primary_key=True)
-        courseNumber: Mapped[int] = mapped_column('course_number',                                               
-                                                            primary_key=True)
-        sectionNumber: Mapped[int] = mapped_column('section_number', Integer,
-                                                  nullable=False, primary_key=True)
-        semester: Mapped[str] = mapped_column('semester', String(10), nullable=False,
-                                                            primary_key=True)
-        sectionYear: Mapped[int] = mapped_column('section_year', Integer, nullable=False,
-                                                            primary_key=True)
-        # Rest of columns (Non primary)
-        building: Mapped[str] = mapped_column('building', String(6), nullable=False)
-        room: Mapped[int] = mapped_column('room', Integer, nullable=False)
-        schedule: Mapped[str] = mapped_column('schedule', String(6)) # Not mandatory?
-        startTime: Mapped[Time] = mapped_column('start_time', Time) # Not mandatory either?
-        instructor: Mapped[str] = mapped_column('instructor', String(80), nullable=False)
+class Section(Base):
+    __tablename__ = table_name  # Give SQLAlchemy the name of the table.
+    # PRIMARY KEYS
+    departmentAbbreviation: Mapped[str] = mapped_column('department_abbreviation', primary_key=True)
+    courseNumber: Mapped[int] = mapped_column('course_number', primary_key=True)
+    sectionNumber: Mapped[int] = mapped_column('section_number', Integer, nullable=False, primary_key=True)
+    semester: Mapped[str] = mapped_column('semester', String(10), nullable=False, primary_key=True)
+    sectionYear: Mapped[int] = mapped_column('section_year', Integer, nullable=False, primary_key=True)
+    # Rest of columns (Non primary)
+    building: Mapped[str] = mapped_column('building', String(6), nullable=False)
+    room: Mapped[int] = mapped_column('room', Integer, nullable=False)
+    schedule: Mapped[str] = mapped_column('schedule', String(6)) # Not mandatory?
+    startTime: Mapped[Time] = mapped_column('start_time', Time) # Not mandatory either?
+    instructor: Mapped[str] = mapped_column('instructor', String(80), nullable=False)
+    # Define relationships - tables populates course? bidirectional
+    course: Mapped["Course"] = relationship(back_populates="sections")
 
-        # Define relationships - tables populates course?
-        course: Mapped["Course"] = relationship(back_populates="sections")
-
-        # Constraints
-        __table_args__ = (
-            # Canidate key 1: room cannot be occupied by more than one section at the same time, 
-            # Canidate key 2: instructor can't teach two sections at same time
-            UniqueConstraint("section_year", "semester", "schedule", "start_time", "building", "room", name="section_uk_01"),
-            UniqueConstraint("section_year", "semester", "schedule", "start_time", "instructor", name="section_uk_02"), 
-            # Ensure valid input
-            CheckConstraint(semester.in_(["Fall", "Spring", "Winter", "Summer I", "Summer II"])),
-            CheckConstraint(schedule.in_(["MW", "TuTh", "MWF", "F", "S"])),
-            CheckConstraint(building.in_(["VEC", "ECS", "EN2", "EN3", "EN4", "ET", "SSPA"])),
-            # Course (Parent) contains two primary keys. Referencing mapped_column and not attribute here
-            ForeignKeyConstraint([departmentAbbreviation, courseNumber], [Course.departmentAbbreviation, Course.courseNumber])
-        )
-        
-        # "Constructor" for Section
-        def __init__(self, course: Course, sectionNumber: int, semester: str, sectionYear: int,  
-                         building: str, room: int, schedule: str, startTime: Time, instructor: str ):
-            self.sectionNumber = sectionNumber
-            self.semester = semester
-            self.sectionYear = sectionYear
-            self.building = building
-            self.room = room
-            self.schedule = schedule
-            self.startTime = startTime
-            self.instructor = instructor
+    # Constraints
+    __table_args__ = (
+        # Canidate key 1: room cannot be occupied by more than one section at the same time, 
+        # Canidate key 2: instructor can't teach two sections at same time
+        UniqueConstraint("section_year", "semester", "schedule", "start_time", "building", "room", name="section_uk_01"),
+        UniqueConstraint("section_year", "semester", "schedule", "start_time", "instructor", name="section_uk_02"), 
+        # Ensure valid input
+        CheckConstraint(semester.in_(["Fall", "Spring", "Winter", "Summer I", "Summer II"])),
+        CheckConstraint(schedule.in_(["MW", "TuTh", "MWF", "F", "S"])),
+        CheckConstraint(building.in_(["VEC", "ECS", "EN2", "EN3", "EN4", "ET", "SSPA"])),
+        # Course (Parent) contains two primary keys. Referencing mapped_column and not attribute here
+        ForeignKeyConstraint([departmentAbbreviation, courseNumber], [Course.departmentAbbreviation, Course.courseNumber])
+    )
+    
+    # "Constructor" for Section
+    def __init__(self, course: Course, sectionNumber: int, semester: str, sectionYear: int,  
+                     building: str, room: int, schedule: str, startTime: Time, instructor: str ):
+        self.sectionNumber = sectionNumber
+        self.semester = semester
+        self.sectionYear = sectionYear
+        self.building = building
+        self.room = room
+        self.schedule = schedule
+        self.startTime = startTime
+        self.instructor = instructor
 
 
 
