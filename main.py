c1db081987d6bcb9c539e025ad73a0820837bdea
morbid240 <162398707+morbid240@users.noreply.github.com>
2024-06-12 06:02:27 -0700

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,9 @@ from db_connection import engine, Session
 from orm_base import metadata
 # Note that until you import your SQLAlchemy declarative classes, such as Student, Python
 # will not execute that code, and SQLAlchemy will be unaware of the mapped table.
-from Department import add_department, select_department, delete_department
+from Department import add_department, select_department, delete_department, list_departments
+from Course import add_course, select_course, delete_course, list_courses
+from Section import add_section, select_section, delete_section
 from Option import Option
 from Menu import Menu
 # Poor man's enumeration of the two available modes for creating the tables
